1fdd3086eea3dd62353ffbfd587f6fd2bc0ff512
Anthony DiFalco <aodifalco@gmail.com>
2022-06-21 14:51:38 -0700

    add click event to display full entry text for each entry individually
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Journal } from "./journal.js";
+import { JournalBook } from "./journal.js";
+
+let journalBook = new JournalBook;
 
 $(document).ready(function() {
   $("form#journal-entry").submit(function(event){
     event.preventDefault();
-    const date = $("input:text")
+    const title = $("#title").val();
+    const date = $("#date").val();
+    const entry = $("#entry").val();
+
+    let journalEntry = new Journal(title, date, entry);
+    journalBook.addEntry(journalEntry);
+    $("#entries").append("<br><strong>" + journalEntry.title + "</strong>");
+    $("#entries").append("<p>" + journalEntry.date + "</p>");
+    $("#entries").append("<div id=" + journalEntry.id + ">" + journalEntry.getTeaser() + "</div><br>");
+
+    $("#entries").append("<p>Word count: " + journalEntry.numberOfWords() + "</p>");
+    $("#entries").append("<p>Vowel count: " + journalEntry.vowels() + "</p>");
+    $("#entries").append("<p>Consonant count: " + journalEntry.consonants() + "</p>");
+    
+    $("#"+journalEntry.id).click(function(event){
+      event.preventDefault();
+      journalEntry.show();
+    });
   });
 });
--- src/journal.js
+++ src/journal.js
@@ -1,20 +1,42 @@
-function Journal(title, date, entry) {
+import $ from 'jquery';
+
+export function JournalBook() {
+  this.entries = {};
+  this.currentId = 0;
+}
+
+JournalBook.prototype.addEntry = function(entry) {
+  entry.id = this.assignId();
+  this.entries[entry.id] = entry;
+  console.log(this);
+};
+JournalBook.prototype.assignId = function() {
+  this.currentId += 1;
+  console.log(this.currentId);
+  return this.currentId;
+};
+
+export function Journal(title, date, entry) {
   this.title = title;
   this.date = date;
   this.entry = entry;
 }
 
 Journal.prototype.getTeaser = function() {
-  let firstSentence = this.entry.split(" ")
+  let firstSentence = this.entry.split(" ");
   firstSentence =  firstSentence.slice(0,7);
   console.log(firstSentence); // test to return first eight words of the journal entry. 
-  return firstSentence;
-}
+  return firstSentence.join(" ");
+};
+
+Journal.prototype.show = function() {
+  $("#" + this.id + "").text(this.entry);
+};
 
 Journal.prototype.numberOfWords = function() {
   let words = this.entry.split(" ");
   return words.length; // test to return the number of words in the entire entry.
-}
+};
 
 Journal.prototype.vowels = function() {
   const vowels = ['a','e','i','o','u'];
@@ -27,7 +49,7 @@ Journal.prototype.vowels = function() {
   }
   console.log(vowelCount);
   return vowelCount;
-}
+};
 
 Journal.prototype.consonants = function() {
   const consonants = 'bcdfghjklmnpqrstvwxyz'.split("");
@@ -40,6 +62,7 @@ Journal.prototype.consonants = function() {
   }
   console.log(consonantCount);
   return consonantCount;
-}
+};
+
 
 // let journal1 = new Journal("day1", "6/21/22", "The quick brown fox jumped over the lazy dog.");
