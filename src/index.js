913da4f1c21c956a6a4b9f29213154592188f902
Anthony DiFalco <aodifalco@gmail.com>
2022-06-21 11:52:53 -0700

    add methods for journal object in journal.js
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-
+  $("form#journal-entry").submit(function(event){
+    event.preventDefault();
+    const date = $("input:text")
+  });
 });
--- src/journal.js
+++ src/journal.js
@@ -5,10 +5,41 @@ function Journal(title, date, entry) {
 }
 
 Journal.prototype.getTeaser = function() {
-  const firstSentence = this.entry.split(" ")
+  let firstSentence = this.entry.split(" ")
   firstSentence =  firstSentence.slice(0,7);
-  console.log(firstSentence);
+  console.log(firstSentence); // test to return first eight words of the journal entry. 
   return firstSentence;
 }
 
-let journal1 = new Journal("day1", "6/21/22", "The quick brown fox jumped over the lazy dog.");
+Journal.prototype.numberOfWords = function() {
+  let words = this.entry.split(" ");
+  return words.length; // test to return the number of words in the entire entry.
+}
+
+Journal.prototype.vowels = function() {
+  const vowels = ['a','e','i','o','u'];
+  let letters = this.entry.split("");
+  let vowelCount = 0;
+  for(let i = 0; i < letters.length; i++){
+    if(vowels.includes(letters[i].toLowerCase())){
+      vowelCount++;
+    }
+  }
+  console.log(vowelCount);
+  return vowelCount;
+}
+
+Journal.prototype.consonants = function() {
+  const consonants = 'bcdfghjklmnpqrstvwxyz'.split("");
+  let letters = this.entry.split("");
+  let consonantCount = 0;
+  for(let i = 0; i < letters.length; i++){
+    if(consonants.includes(letters[i].toLowerCase())){
+      consonantCount++;
+    }
+  }
+  console.log(consonantCount);
+  return consonantCount;
+}
+
+// let journal1 = new Journal("day1", "6/21/22", "The quick brown fox jumped over the lazy dog.");
